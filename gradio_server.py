44c450d56aa13cf19e4093852940ec67570390f5
Zoraaaa <rongyu923826@outlook.com>
2024-06-23 19:18:42 -0700

    fixed bug add another tab page

--- gradio_server.py
+++ gradio_server.py
@@ -44,11 +44,13 @@ Make sure to be precise and follow the output format strictly.
 
 """
 
-def init(novel_type, description, language, request: gr.Request):
+def init(novel_type, description, language, save_story, request: gr.Request):
+    out_file = None
+    if save_story == "Yes":
+        out_file = f"{novel_type}_{description}_{language}.txt"
+
     cookie = request.headers.get('cookie', None)
     if cookie is None:
-        # Handle the case where the cookie is not present
-        # print("Cookie not found in the request headers????")
         cookie = ""
     else:
         cookie = request.headers['cookie']
@@ -57,13 +59,9 @@ def init(novel_type, description, language, request: gr.Request):
     if novel_type == "":
         novel_type = "Science Fiction"
     global _CACHE
-    # print("what is cache", _CACHE)
-    # cookie = request.headers['cookie']
-    # cookie = cookie.split('; _gat_gtag')[0]
-    # prepare first init
-    out_file = f"{novel_type}_{description}_{language}.txt"
+
     init_paragraphs = get_init(text=init_prompt(novel_type, description, language),response_file=out_file)
-    # print(init_paragraphs)
+    
     start_input_to_human = {
         'output_paragraph': init_paragraphs['Paragraph 3'],
         'input_paragraph': '\n\n'.join([init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']]),
@@ -82,30 +80,26 @@ Paragraphs:
 {start_input_to_human['input_paragraph']}"""
     long_memory = parse_instructions([init_paragraphs['Paragraph 1'], init_paragraphs['Paragraph 2']])
     # short memory, long memory, current written paragraphs, 3 next instructions
+    print("inital", written_paras)
     return start_input_to_human['output_memory'], long_memory, written_paras, init_paragraphs['Instruction 1'], init_paragraphs['Instruction 2'], init_paragraphs['Instruction 3']
 
-def step(novel_type, description, language, short_memory, long_memory, instruction1, instruction2, instruction3, current_paras, request: gr.Request,):
-    out_file = f"{novel_type}_{description}_{language}.txt"
+def step(novel_type, description, language, short_memory, long_memory, instruction1, instruction2, instruction3, current_paras, save_story, request: gr.Request,):
+    out_file = None
+    if save_story == "Yes":
+        out_file = f"{novel_type}_{description}_{language}.txt"
     
     if current_paras == "":
         return "", "", "", "", "", ""
     global _CACHE
-    # print(list(_CACHE.keys()))
-    # print(request.headers.get('cookie'))
-    # cookie = request.headers['cookie']
-    # cookie = cookie.split('; _gat_gtag')[0]
-    # print(cookie)
+    print("control step request", request)
     cookie = request.headers.get('cookie', None)
     if cookie is None:
         # Handle the case where the cookie is not present
-        # print("Cookie not found in the request headers????")
         cookie = ""
     else:
         cookie = request.headers['cookie']
         cookie = cookie.split('; _gat_gtag')[0]
-    # print("what is globle cache", _CACHE)
     cache = _CACHE[cookie]
-    # print("what is cache", cache)
 
     if "writer" not in cache:
         start_input_to_human = cache["start_input_to_human"]
@@ -137,31 +131,29 @@ def step(novel_type, description, language, short_memory, long_memory, instructi
         writer.input = human.output
         writer.step()
 
-    # long_memory = [[v] for v in writer.long_memory]
     long_memory = parse_instructions(writer.long_memory)
     # short memory, long memory, current written paragraphs, 3 next instructions
     return writer.output['output_memory'], long_memory, current_paras + '\n\n' + writer.output['input_paragraph'], human.output['output_instruction'], *writer.output['output_instruction']
 
 
-def controled_step(language, short_memory, long_memory, selected_instruction, current_paras, out_file, request: gr.Request, ):
+def controled_step(novel_type, description, language, short_memory, long_memory, selected_instruction, current_paras, save_story, request: gr.Request, ):
+    out_file = None
+    if save_story == "Yes":
+        out_file = f"{novel_type}_{description}_{language}.txt"
+    
     if current_paras == "":
         return "", "", "", "", "", ""
     global _CACHE
-    # print(list(_CACHE.keys()))
-    # print(request.headers.get('cookie'))
+    print("control step request", request)
     cookie = request.headers.get('cookie', None)
     if cookie is None:
         # Handle the case where the cookie is not present
-        print("Cookie not found in the request headers????")
         cookie = ""
     else:
         cookie = request.headers['cookie']
         cookie = cookie.split('; _gat_gtag')[0]
-    # cookie = request.headers['cookie']
-    # cookie = cookie.split('; _gat_gtag')[0]
     cache = _CACHE[cookie]
 
-    # print("what is cache", cache)
     if "writer" not in cache:
         start_input_to_human = cache["start_input_to_human"]
         start_input_to_human['output_instruction'] = selected_instruction
@@ -189,8 +181,9 @@ def controled_step(language, short_memory, long_memory, selected_instruction, cu
         writer.input = human.output
         writer.step()
     # long_memory = parse_instructions(writer.long_memory)
+    long_memory = parse_instructions(writer.long_memory)
     # short memory, long memory, current written paragraphs, 3 next instructions
-    return writer.output['output_memory'], parse_instructions(writer.long_memory), current_paras + '\n\n' + writer.output['input_paragraph'], *writer.output['output_instruction']
+    return writer.output['output_memory'], long_memory, current_paras + '\n\n' + writer.output['input_paragraph'], selected_instruction, *writer.output['output_instruction']
 
 
 # SelectData is a subclass of EventData
@@ -200,6 +193,15 @@ def on_select(instruction1, instruction2, instruction3, evt: gr.SelectData):
     return selected_plan
 
 with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="default") as demo:
+    gr.Markdown(
+        """
+    # Auto-Generation & Select-Generation
+    # Novel Type is REQUIRED
+    # Topic is not required but good to have.
+    # Default language is English. If you want the story to be other language, please specify.
+    # Must Initial Novel FIRST.
+    """)
+
     with gr.Tab("Auto-Generation"):
         with gr.Column():
             with gr.Row():
@@ -207,11 +209,12 @@ with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="d
                     label="Novel Type", placeholder="e.g. science fiction")
                 description = gr.Textbox(label="Topic")
                 language = gr.Textbox(label="Language")
-            btn_init = gr.Button(
-                "Init Novel Generation", elem_id="init_button")
+                save_story = gr.Radio(choices=["Yes", "No"], label="Save Story")
             gr.Examples(["Science Fiction", "Romance", "Mystery", "Fantasy",
                         "Historical", "Horror", "Thriller", "Western", "Young Adult"],
                         inputs=[novel_type], elem_id="example_selector")
+            btn_init = gr.Button(
+                "Init Novel Generation", elem_id="init_button")
             written_paras = gr.Textbox(
                 label="Written Paragraphs (editable)", lines=21)
 
@@ -230,14 +233,63 @@ with gr.Blocks(title="RecurrentGPT", css="footer {visibility: hidden}", theme="d
                 label="Instruction 3 (editable)", lines=4)
             selected_plan = gr.Textbox(
                 label="Revised Instruction (from last step)", lines=2)
-        # out_file = f"{novel_type}_{description}_{language}.txt"
         btn_step = gr.Button("Next Step", elem_id="step_button")
-        btn_init.click(init, inputs=[novel_type, description, language], outputs=[
+        btn_init.click(init, inputs=[novel_type, description, language, save_story], outputs=[
             short_memory, long_memory, written_paras, instruction1, instruction2, instruction3])
-        btn_step.click(step, inputs=[novel_type, description, language, short_memory, long_memory, instruction1, instruction2, instruction3, written_paras], outputs=[
+        btn_step.click(step, inputs=[novel_type, description, language, short_memory, long_memory, instruction1, instruction2, instruction3, save_story, written_paras], outputs=[
             short_memory, long_memory, written_paras, selected_plan, instruction1, instruction2, instruction3])
+    
+    # new tab
+    with gr.Tab("Select-Generation"):
+        with gr.Column():
+            with gr.Row():
+                novel_type = gr.Textbox(
+                    label="Novel Type", placeholder="e.g. science fiction")
+                description = gr.Textbox(label="Topic")
+                language = gr.Textbox(label="Language")
+                save_story = gr.Radio(choices=["Yes", "No"], label="Save Story")
+            gr.Examples(["Science Fiction", "Romance", "Mystery", "Fantasy",
+                        "Historical", "Horror", "Thriller", "Western", "Young Adult"],
+                        inputs=[novel_type], elem_id="example_selector")
+            btn_init = gr.Button(
+                "Init Novel Generation", elem_id="init_button")
+            written_paras = gr.Textbox(
+                label="Written Paragraphs (editable)", lines=21)
 
-    demo.launch()
+        with gr.Column():
+            gr.Markdown("### Memory Module")
+            short_memory = gr.Textbox(
+                label="Short-Term Memory (editable)", lines=3)
+            long_memory = gr.Textbox(
+                label="Long-Term Memory (editable)", lines=6)
+            gr.Markdown("### Instruction Module")
+            instruction1 = gr.Textbox(
+                label="Instruction 1 (editable)", lines=4)
+            instruction2 = gr.Textbox(
+                label="Instruction 2 (editable)", lines=4)
+            instruction3 = gr.Textbox(
+                label="Instruction 3 (editable)", lines=4)
+            last_step = gr.Textbox(
+                label="Instruction from last step", lines=2)
+        with gr.Column():
+            with gr.Column(scale=1, min_width=100):
+                            selected_plan = gr.Radio(["Instruction 1", "Instruction 2", "Instruction 3"], label="selected_plan")
+                                                    #  info="Select the instruction you want to revise and use for the next step generation.")
+            with gr.Column(scale=3, min_width=300):
+                            selected_instruction = gr.Textbox(
+                                label="Selected Instruction (editable)", max_lines=5, lines=5)
+
+        btn_step = gr.Button("Next Step", elem_id="step_button")
+        btn_init.click(init, inputs=[novel_type, description, language, save_story], outputs=[
+            short_memory, long_memory, written_paras, instruction1, instruction2, instruction3])
+        btn_step.click(controled_step, inputs=[novel_type, description, language, short_memory, long_memory, selected_instruction, save_story, written_paras], outputs=[
+            short_memory, long_memory, written_paras, last_step, instruction1, instruction2, instruction3])
+        selected_plan.select(on_select, inputs=[
+                             instruction1, instruction2, instruction3], outputs=[selected_instruction])
+
+    demo.queue(max_size=20)
+    demo.launch(max_threads=1, inbrowser=True, share=True)
+    #demo.queue(concurrency_count=1)
 
 if __name__ == "__main__":
     demo.launch(server_port=8005, share=True,
